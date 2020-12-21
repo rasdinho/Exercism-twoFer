b925b186f78ca2acf847e0e7cd081430aa8ff14b
rasdinho <ras.dinho7@gmail.com>
2020-12-21 12:19:42 -0500

    two-fer

---
+++ two-fer.js
@@ -0,0 +1,24 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+// export const twoFer = () => {
+//   throw new Error('no name given');
+  
+// };
+
+// export const twoFer = (name = 'you') => `One for ${name}, one for me.`;
+
+
+export const twoFer = (name = 'you') => {
+  return `One for ${name}, one for me.`
+}
+
+// export const twoFer = (name = 'Alice') => {
+//   return `One for ${name}, one for me.`
+// }
+
+// export const twoFer = (name = 'Bob') => {
+//   return `One for ${name}, one for me.`
+// }
